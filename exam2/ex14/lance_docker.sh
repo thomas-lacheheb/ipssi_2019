e8629abf859d7a47ec1e4b1f66ceaf9501616204
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2019-11-10 19:20:35 +0100

    Ajout cmd linux et windows ex14

--- exam2/ex14/lance_docker.sh
+++ exam2/ex14/lance_docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#Sous linux
 docker run -it -v /tmp:/out --name container1 ipssi/ex14:1 /bin/sh
+
+#Sous windows
+#docker run -it -v /c/Users/thoma/AppData/local/tmp:/out --name container1 ipssi/ex14:1 /bin/sh
