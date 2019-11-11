0709123abce10b4f8857fa75b6d152a453ccd946
Lacheheb Thomas <thomas.lacheheb@hotmail.fr>
2019-11-11 14:17:33 +0100

    Maj format date ex04

--- exam2/ex04/logs.py
+++ exam2/ex04/logs.py
@@ -3,8 +3,8 @@ import datetime
 
 def logthis(text):
     f = open("python.log", "a")
-    date = datetime.datetime.now().strftime('%d-%b-%Y %H:%M:%S')
-    #date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    #date = datetime.datetime.now().strftime('%d-%b-%Y %H:%M:%S')
+    date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     fulltext = date + " " + str(text)
     f.write(fulltext+"\n")
     f.close()
