539bd5c17524365b66c7a077190e5f913c058dee
Thomas Lacheheb <thomas.lacheheb@hotmail.fr>
2019-12-09 23:50:17 +0100

    ajout ansible nginx

--- exam3/noel.py
+++ exam3/noel.py
@@ -2,7 +2,12 @@
 
 import sys
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
+from tree import show_tree
+
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
 
 def show_noel(allArg):
     if len(allArg) > 1:
@@ -11,6 +16,8 @@ def show_noel(allArg):
     else:
         theDate = datetime.now()
 
+    allCalendarToPrint = ""
+
     yearForChristmas = datetime.now().year
     # Définit si noël aura lieu cette année ou l'année prochaine (Si on est le 26 Décembre par exemple)
     if theDate.strftime('%Y-%m-%d') > str(datetime.now().year)+"-12-25":
@@ -18,16 +25,21 @@ def show_noel(allArg):
     dateToNextChristmas = datetime.strptime(str(yearForChristmas)+"-12-25", "%Y-%m-%d")
     daysBfChristmas = str(abs((dateToNextChristmas - theDate).days))+" days before christmas\n"
 
+    for i in daterange(theDate, dateToNextChristmas):
+        # c = calendar.TextCalendar(calendar.MONDAY)
+        # allCalendarToPrint += c.formatmonth(theDate.year, theDate.month, 5, 1)
+        # print(allCalendarToPrint)
+        print(i.strftime('%Y-%m-%d').timedelta(days=30))
+
     c = calendar.TextCalendar(calendar.MONDAY)
     firstCalendar = c.formatmonth(theDate.year, theDate.month, 5, 1)
     firstCalendar = firstCalendar.replace(str(theDate.day), "("+str(theDate.day)+")", 1)
 
     c = calendar.TextCalendar(calendar.MONDAY)
-    secondCalendar = c.formatmonth(yearForChristmas, 12, 5, 1)
-    secondCalendar = secondCalendar.replace(" 25 ", "[25]", 1)
+    decemberCalendar = c.formatmonth(yearForChristmas, 12, 5, 1)
+    decemberCalendar = decemberCalendar.replace(" 25 ", "[25]", 1)
 
-    sapin = """
-        x
+    sapin = """        x
        xxx
       xxxxx
      xxxxxxx
@@ -38,12 +50,13 @@ def show_noel(allArg):
        xxx
        xxx
        xxx
-       xxx
-        """
+       xxx"""
+
     if theDate.strftime('%Y-%m-%d') == str(theDate.year)+"-12-25":
         return sapin
     else:
-        return daysBfChristmas+firstCalendar+secondCalendar
+        return True
+        # return daysBfChristmas+firstCalendar+decemberCalendar
 
 if __name__ == "__main__":
     print(show_noel(sys.argv))
